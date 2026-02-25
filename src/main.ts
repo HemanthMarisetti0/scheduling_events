08c357035fdaf3746b36eac1205892d351367897
MARISETTI HEMANTH KUMAR <hemanthmarisetti555@gmail.com>
2026-02-25 10:34:37 +0530

    fix: added cors for server apis

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,23 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        process.env.FRONTEND_DEV_URL,
+        process.env.FRONTEND_PROD_URL,
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('AI Scheduler API')
